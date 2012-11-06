7825d065e2eff15ffb52c562795ddb86a5ff638c
Tobin Fricke <tobin@pas.rochester.edu>
2012-11-06 14:20:23 +0100

    TTFSS model from mevans

---
+++ Servo/MEvans/getTFs.m
@@ -0,0 +1,121 @@
+% [hOL, hEOM, hPZT] = getTFs(param)
+%
+% Example (parameter version 3 with the anti-boost enabled):
+% [hOL, hCross, hEOM, hPZT, info] = getTTFSS(paramTTFSS(3, f), true, f);
+
+function [hOL, hCross, hEOM, hPZT, info] = getTTFSS(param, enableAB, f)
+  
+  % s-domain
+  f = f(:);
+  s = 2i * pi * f;
+  
+  % extract everything from the parameter struct
+  struct2workspace(param);
+  
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% RF Input Stage
+  filtRF = filtZPGQ(zerosRF, polesRF, gainRF, 0);
+      
+  if enableAB
+    filtRF = filtProd(filtRF, filtZPGQ(zerosAB, polesAB, 1, Inf));
+  end
+  hRF = sresp(filtRF, f);
+  
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Common Path
+  % U3 and VGA
+  hComm = hRF * (R4 / R3) * 10^(commGain / 20);
+      
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Fast Path
+  % U8 and U9
+  hU8 = parRC(R30, C36, s) ./ R35;
+  hU9 = parRC(R31, C37, s) ./ R36;
+  hU7 = R29 ./ R37_R32;
+  
+  % output filter
+  hOut = 1 ./ (s .* C51 .* serRC(R46, C51, s));
+  
+  % total, with VGA
+  hPZT = hComm .* 10.^(fastGain / 20) .* hU8 .* hU9 .* hU7;
+
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% EOM Path
+  % U4
+  hU4 = parRR(serRC(R17, C15, s), R19) ./ serRC(R22, C23, s);
+  hU5 = parRR(serRC(R18, C18, s), R20) ./ serRC(R24, C24, s);
+      
+  % U6
+  zPA = ((s * L1 + R15) ./ (s * L1)) .* ...
+    (serRC(R14, C11, s) + parRR(R15, s * L1));
+
+  hU6 = R21 ./ serRC(R25, C25, s);
+  hU6pa = R21 ./ zPA;
+  
+  % total
+  hU456 = hU4 .* hU5 .* hU6;
+  hEOM = hComm .* (hU456 + hU6pa);
+
+  % high voltage
+  hHV = parRR(serRC(hv.R45, hv.C25, s), hv.R47) ./ serRC(hv.R42, hv.C22, s);
+      
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Total Open-Loop
+  hDelayPZT = exp(-s * delayPZT);
+  hDelayEOM = exp(-s * delayEOM);
+  hOL(:, 1) = hDelayPZT .* coefPFD .* coefPZT .* hOut .* hPZT ./ s;
+  hOL(:, 2) = hDelayEOM .* coefPFD .* coefEOM .* hHV .* hEOM;
+  hOL(:, 3) = hOL(:, 1) + hOL(:, 2);
+
+  hCross = hOL(:, 1) ./ (1 + hOL(:, 2));
+  
+  % other stuff
+  info = workspace2struct;
+  
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%
+function h = parRC(R, C, s)
+  h = 1 ./ ((1 ./ R) + s .* C);
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%
+function h = serRC(R, C, s)
+  h = R + 1 ./ (s .* C);
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%
+function h = parRR(R1, R2)
+  h = 1 ./ ((1 ./ R1) + (1 ./ R2));
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% struct2workspace(s)
+%   assigns all of the argument's fields to the caller's workspace
+
+function struct2workspace(s)
+
+  % loop over structs
+  for m = 1:numel(s)
+    
+    % get field names
+    names = fieldnames(s(m));
+    
+    % assign them in the caller's workspace
+    for n = 1:numel(names)
+      assignin('caller', names{n}, s(m).(names{n}));
+    end
+    
+  end
+  
+end
+
+% s = workspace2struct
+%   store variables in the current workspace in the returned struct
+
+function s = workspace2struct
+  
+  % get the names of variables in the calling function
+  names = evalin('caller', 'who');
+  
+  % assign their values to fields in a struct
+  for n = 1:numel(names)
+    s.(names{n}) = evalin('caller', names{n});
+  end
+  
+end
---
+++ Servo/MEvans/paramTTFSS.m
@@ -0,0 +1,171 @@
+% param = paramTTFSS(ver)
+
+function param = paramTTFSS(ver, f)
+  
+  if nargin < 1
+    ver = 0;
+  end
+  if nargin > 1
+    % s-domain
+    f = f(:);
+    s = 2i * pi * f;
+  end
+  
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  % Default Values
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% RF Input Stage
+  param.polesRF = [3e6 0; 1.6e6 1.2; 3e5 0]; % tweaked to match measurements
+  param.zerosRF = [3e6 0; 42e6 100; 6e5 0];
+  param.gainRF = 2; % should be 4?
+  
+  param.polesAB = [200e3 0; 20e3 0];
+  param.zerosAB = [273 0; 30e3 0];
+  
+  param.coefPFD = 0.3;  % phase-frequency descriminate V/rad
+  
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Common Path
+  % U3 gain of 3.16 = 10dB
+  param.R3 = 124;
+  param.R4 = 392;
+  
+  param.commGain = 20; % in dB (650 => 16db)
+  
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Fast Path
+  param.fastGain = 22; % in dB (800 => 22db)
+  
+  % U8
+  param.R35 = 560; % input
+  param.R30 = 4.87e3; % FB
+  param.C36 = 3.3e-9; % FB
+  
+  % U9
+  param.R36 = 750; % input
+  param.R31 = 3.09e3; % FB
+  param.C37 = 1.5e-9; % FB
+  
+  % U7
+  param.R37_R32 = 1e3; % input network effective impedence
+  param.R29 = 5.6e3;   % FB
+  
+  % output filter
+  param.R46 = 15.8e3;
+  param.C51 = 1e-6;
+  
+  % PZT coefficent in Hz/V
+  param.coefPZT = 3.5e6;
+  param.delayPZT = 1200e-9;
+  
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% EOM Path
+  % U4
+  param.C23 = 1e-6; % input
+  param.R22 = 1.1e3; % input
+  param.R19 = 24.9e3; % FB
+  param.C15 = 3.3e-9; % FB
+  param.R17 = 0e3; % FB
+  
+  % U5
+  param.C24 = 1e-6; % input
+  param.R24 = 1.3e3; % input
+  param.R20 = 47e3; % FB
+  param.C18 = 3.3e-9; % FB
+  param.R18 = 1.5e3; % FB
+  
+  % U6
+  param.C11 = 330e-12; % input (pass around)
+  param.R14 = 3.01e3; % input (pass around)
+  param.L1 = 200e-6; % input (pass around)
+  param.R15 = 100; % input (pass around)
+  
+  param.C25 = 1e-6; % input
+  param.R25 = 499; % input
+  param.R21 = 10e3; % FB
+  
+  % high voltage output (PA85)
+  param.hv.C22 = 221e-9; % and C21
+  param.hv.R42 = 3.3e3;
+  
+  param.hv.R47 = 100e3;
+  param.hv.R45 = 1e3;
+  param.hv.C25 = 47e-12;
+      
+  % EOM coefficent in rad/V
+  param.coefEOM = 10e-3;
+  param.delayEOM = 0e-9;
+  
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  % Version Specific Values
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  
+  switch ver
+    case 0 % default
+    case 1
+      % low gain mode - PZT only
+      
+      % remove some anti-boost (so PZT path works better with AB on)
+      param.zerosAB(1, :) = [4 * 273 0]; % R22, R24 = 124k -> 31k
+      
+      %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Common Path
+      % U3 - now 1
+      %param.R3 = 392;
+      %param.R4 = 392;
+      
+      %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Fast Path
+      % move PZT poles up, allowing wider band PZT
+      param.R30 = 2e3 + parRC(20e3, 33e-9, s); % FB, U8
+      %param.R30 = 2e3; % FB, U8
+      param.C36 = 560e-12; % FB, U8
+      
+      %param.R31 = 3e3 + parRC(10e3, 10e-9, s); % FB, U9
+      %param.C37 = 1e-12; % FB, U9
+
+      param.commGain = 0; % in dB
+      param.fastGain = 0; % in dB
+      
+      %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% EOM Path
+      param.R19 = 2e3; % FB, U4 (move pole out)
+      param.C23 = 100e-9;  % higher AC couple
+  
+    case 2
+      % in high gain mode
+      param = paramTTFSS(1, f);
+
+      param.commGain = 20; % in dB
+      param.fastGain = 20; % in dB
+    case 3
+      % low gain mode - PZT only
+      
+      % remove some anti-boost (so PZT path works better with AB on)
+      param.zerosAB(1, :) = [40 * 273 0]; % R22, R24 = 124k -> 31k
+      
+      %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Common Path
+      % U3 - now 1
+      %param.R3 = 392;
+      %param.R4 = 392;
+      
+      %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Fast Path
+      % move PZT poles up, allowing wider band PZT
+      param.R30 = 1e3 + parRC(20e3, 33e-9, s); % FB, U8
+      %param.R30 = 2e3; % FB, U8
+      param.C36 = 560e-12; % FB, U8
+      
+      %param.R31 = 3e3 + parRC(10e3, 10e-9, s); % FB, U9
+      %param.C37 = 1e-12; % FB, U9
+
+      param.commGain = 20; % in dB
+      param.fastGain = 30; % in dB
+      
+      %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% EOM Path
+      param.R19 = 2e3; % FB, U4 (move pole out)
+  
+    otherwise
+      error('Unknown version %d', ver)
+  end
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%
+function h = parRC(R, C, s)
+  h = 1 ./ ((1 ./ R) + s .* C);
+end
+
